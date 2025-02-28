bb9d66709d182040cc8a74756431edbea02f8bc3
James O'Reilly <jp.oreilly@hotmail.com>
2025-02-28 17:19:44 +0000

    Updates

--- Playground/agno/agent-tools.py
+++ Playground/agno/agent-tools.py
@@ -3,7 +3,7 @@ from agno.models.ollama import Ollama
 from agno.tools.duckduckgo import DuckDuckGoTools
 
 agent = Agent(
-    model=Ollama(id="deepsekk-llm:latest"),
+    model=Ollama(id="llama3.2:latest"),
     tools=[DuckDuckGoTools()],
     show_tool_calls=True,
     markdown=True,
